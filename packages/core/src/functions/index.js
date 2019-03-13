69e1b7b2f784854b777438540d891d1a52e1b466
leny <info@flatland.be>
2019-03-13 17:05:44 +0100

    Add failing tests for css functions

---
+++ packages/core/src/functions/index.js
@@ -0,0 +1,69 @@
+/* leny/pwops
+ *
+ * /src/functions/index.js - CSS functions
+ *
+ * coded by leny
+ * started at 13/03/2019
+ */
+
+exports.attr = () => ({});
+exports.blur = () => ({});
+exports.brightness = () => ({});
+exports.calc = () => ({});
+exports.circle = () => ({});
+exports.clamp = () => ({});
+exports.conicGradient = () => ({});
+exports.contrast = () => ({});
+exports.crossFade = () => ({});
+exports.dropShadow = () => ({});
+exports.element = () => ({});
+exports.ellipse = () => ({});
+exports.env = () => ({});
+exports.fitContent = () => ({});
+exports.grayscale = () => ({});
+exports.hsl = () => ({});
+exports.hsla = () => ({});
+exports.hueRotate = () => ({});
+exports.image = () => ({});
+exports.imageSet = () => ({});
+exports.inset = () => ({});
+exports.invert = () => ({});
+exports.leader = () => ({});
+exports.linearGradient = () => ({});
+exports.matrix = () => ({});
+exports.matrix3d = () => ({});
+exports.max = () => ({});
+exports.min = () => ({});
+exports.minmax = () => ({});
+exports.opacity = () => ({});
+exports.perspective = () => ({});
+exports.polygon = () => ({});
+exports.radialGradient = () => ({});
+exports.repeatingLinearGradient = () => ({});
+exports.repeatingRadialGradient = () => ({});
+exports.rgb = () => ({});
+exports.rgba = () => ({});
+exports.rotate = () => ({});
+exports.rotate3d = () => ({});
+exports.rotateX = () => ({});
+exports.rotateY = () => ({});
+exports.rotateZ = () => ({});
+exports.saturate = () => ({});
+exports.scale = () => ({});
+exports.scale3d = () => ({});
+exports.scaleX = () => ({});
+exports.scaleY = () => ({});
+exports.scaleZ = () => ({});
+exports.skew = () => ({});
+exports.skewX = () => ({});
+exports.skewY = () => ({});
+exports.sepia = () => ({});
+exports.targetCounter = () => ({});
+exports.targetCounters = () => ({});
+exports.targetText = () => ({});
+exports.translate = () => ({});
+exports.translate3d = () => ({});
+exports.translateX = () => ({});
+exports.translateY = () => ({});
+exports.translateZ = () => ({});
+exports.cssVar = () => ({}); // translate as var
---
+++ packages/core/test/functions.js
@@ -0,0 +1,100 @@
+/* leny/pwops
+ *
+ * /test/functions.js - Test for css functions
+ *
+ * coded by leny
+ * started at 13/03/2019
+ */
+
+const assert = require("assert");
+
+const cssFunctions = require("../lib");
+
+describe("CSS functions", () => {
+    Object.entries({
+        attr: ["attr", 1, 2],
+        blur: ["blur", 1, 1],
+        brightness: ["brightness", 1, 1],
+        calc: ["calc", 1, 1],
+        circle: ["circle", 1, 1],
+        clamp: ["clamp", 3, 3],
+        conicGradient: ["conic-gradient", 2, Infinity],
+        contrast: ["contrast", 1, 1],
+        crossFade: ["cross-fade", 2, Infinity],
+        dropShadow: ["drop-shadow", 2, 5],
+        element: ["element", 1, 1],
+        ellipse: ["ellipse", 1, 1],
+        env: ["env", 1, 2],
+        fitContent: ["fit-content", 1, 1],
+        grayscale: ["grayscale", 1, 1],
+        hsl: ["hsl", 1, 4],
+        hsla: ["hsla", 1, 4],
+        hueRotate: ["hue-rotate", 1, 1],
+        image: ["image", 1, Infinity],
+        imageSet: ["image-set", 1, Infinity],
+        inset: ["inset", 1, 5],
+        invert: ["invert", 1, 1],
+        leader: ["leader", 1, 1],
+        linearGradient: ["linear-gradient", 2, Infinity],
+        matrix: ["matrix", 6, 6],
+        matrix3d: ["matrix3d", 16, 16],
+        max: ["max", 1, Infinity],
+        min: ["min", 1, Infinity],
+        minmax: ["minmax", 2, 2],
+        opacity: ["opacity", 1, 1],
+        perspective: ["perspective", 1, 1],
+        polygon: ["polygon", 2, Infinity],
+        radialGradient: ["radial-gradient", 2, Infinity],
+        repeatingLinearGradient: ["repeating-linear-gradient", 2, Infinity],
+        repeatingRadialGradient: ["repeating-radial-gradient", 2, Infinity],
+        rgb: ["rgb", 1, 4],
+        rgba: ["rgba", 1, 4],
+        rotate: ["rotate", 1, 1],
+        rotate3d: ["rotate3d", 1, 4],
+        rotateX: ["rotateX", 1, 1],
+        rotateY: ["rotateY", 1, 1],
+        rotateZ: ["rotateZ", 1, 1],
+        saturate: ["saturate", 1, 1],
+        scale: ["scale", 1, 2],
+        scale3d: ["scale3d", 3, 3],
+        scaleX: ["scaleX", 1, 1],
+        scaleY: ["scaleY", 1, 1],
+        scaleZ: ["scaleZ", 1, 1],
+        skew: ["skew", 1, 2],
+        skewX: ["skewX", 1, 1],
+        skewY: ["skewY", 1, 1],
+        sepia: ["sepia", 1, 1],
+        targetCounter: ["target-counter", 2, 3],
+        targetCounters: ["target-counters", 3, 4],
+        targetText: ["target-text", 1, 2],
+        translate: ["translate", 1, 2],
+        translate3d: ["translate3d", 1, 3],
+        translateX: ["translateX", 1, 1],
+        translateY: ["translateY", 1, 1],
+        translateZ: ["translateZ", 1, 1],
+        url: ["url", 1, 1],
+        cssVar: ["var", 1, 1],
+    }).forEach(([func, [name, minArgs, maxArgs]]) => {
+        const [min, max] = [minArgs, maxArgs === Infinity ? 20 : maxArgs];
+        const method = cssFunctions[func];
+
+        describe(`${func}()`, () => {
+            it(`Should return a correct ${name}() when correctly filled`, () => {
+                for (let i = min; i < max; i++) {
+                    const args = Array(i).fill("foo", 0, i);
+
+                    assert.strictEqual(
+                        method(...args),
+                        `${name}(${args.join(", ")})`,
+                    );
+                }
+            });
+
+            it(`Should throw if a wrong amount of arguments is passed to ${func}`, () => {
+                const args = Array(max).fill("foo", 0, max);
+
+                assert.throws(() => method(...args), TypeError);
+            });
+        });
+    });
+});
