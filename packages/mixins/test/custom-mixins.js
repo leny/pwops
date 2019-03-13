a5091029ab70f08a95266dba725c1eaa8a09db0e
leny <info@flatland.be>
2019-03-13 19:24:10 +0100

    Add failing tests for custom mixins

---
+++ packages/mixins/test/custom-mixins.js
@@ -0,0 +1,68 @@
+/* leny/pwops
+ *
+ * /test/custom-mixins.js - Test for custom mixins injection
+ *
+ * coded by leny
+ * started at 13/03/2019
+ */
+
+const assert = require("assert");
+
+const customClearfixMixin = () => ({
+    "&:before,&:after": {
+        content: `""`,
+        display: "table",
+    },
+    "&:after": {
+        clear: "both",
+    },
+});
+
+const {pwop, registerMixin} = require("../lib");
+
+describe("Custom Mixins", () => {
+    registerMixin("clearfix", customClearfixMixin);
+
+    describe("clearfix", () => {
+        it("Custom mixin is available", () => {
+            assert.deepEqual(
+                pwop({
+                    color: "red",
+                    clearfix: true,
+                }),
+                {
+                    color: "red",
+                    "&:before,&:after": {
+                        content: `""`,
+                        display: "table",
+                    },
+                    "&:after": {
+                        clear: "both",
+                    },
+                },
+            );
+        });
+        it("Custom mixin is available in nesting", () => {
+            assert.deepEqual(
+                pwop({
+                    color: "red",
+                    p: {
+                        clearfix: [],
+                    },
+                }),
+                {
+                    color: "red",
+                    p: {
+                        "&:before,&:after": {
+                            content: `""`,
+                            display: "table",
+                        },
+                        "&:after": {
+                            clear: "both",
+                        },
+                    },
+                },
+            );
+        });
+    });
+});
