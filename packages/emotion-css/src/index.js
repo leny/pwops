c46bbc59f347cb1f09da4278a83a587b4cd01d54
leny <info@flatland.be>
2019-03-14 00:14:35 +0100

    Fix emotion/css import

--- packages/emotion-css/src/index.js
+++ packages/emotion-css/src/index.js
@@ -6,7 +6,7 @@
  * started at 13/03/2019
  */
 
-const emotionCSS = require("@emotion/css");
+const emotionCSS = require("@emotion/css").default;
 const {pwop} = require("@pwops/core");
 
 exports.css = obj => emotionCSS(pwop(obj));
