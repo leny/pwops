f4f45f16f48ecc6a38a0dd7ced8cfe930a7b9354
leny <info@flatland.be>
2019-03-13 23:23:36 +0100

    Add emotion-css package

---
+++ packages/emotion-css/src/index.js
@@ -0,0 +1,12 @@
+/* leny/pwops/emotion-css
+ *
+ * /src/index.js - Entry point
+ *
+ * coded by leny
+ * started at 13/03/2019
+ */
+
+const emotionCSS = require("@emotion/css");
+const {pwop} = require("@pwops/core");
+
+exports.css = obj => emotionCSS(pwop(obj));
